32d7d0a012df7e14a8c4cf066b8385e7ee9df61b
ljwoodward <ljwoodward.code@gmail.com>
2021-02-16 20:37:12 +0000

    refactored ui

--- ui.py
+++ ui.py
@@ -8,7 +8,7 @@ def terminal_size():
     return tw, th
 
 WIDTH, HEIGHT = terminal_size()
-SCROLL_SPEED = 4 # lines per second
+SCROLL_SPEED = 6 # lines per second
 
 def line(char):
     return char*WIDTH
@@ -31,15 +31,15 @@ def vSpace(n):
         t += '\n'
     return t
 
-def scroll(seconds):
-    for i in range(seconds*SCROLL_SPEED):
-        print()
+def scroll(text):
+    spl = text.split('\n')
+    for i in spl:
+        print(i)
         time.sleep(1/SCROLL_SPEED)
 
 def textBox(text, char):
     return '\n'.join([line(char), side(char), innerText(text, char), side(char), line(char)])
 
 def opening():
-    scroll(3)
-    print(textBox('Wilkommen in QuizzyGillespie!', '*'))
-    scroll(3)
+    txt = vSpace(3) + textBox('Wilkommen in QuizzyGillespie!', '*') + vSpace(3)
+    scroll(txt)
